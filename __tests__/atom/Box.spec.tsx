9ca92942a8dc5923badaa14dd8efa3a93e8ca3a9
adeyahya <adeyahyaprasetyo@gmail.com>
2021-03-10 23:52:49 +0700

    bump version

--- __tests__/atom/Box.spec.tsx
+++ __tests__/atom/Box.spec.tsx
@@ -4,5 +4,5 @@ import {Box} from '@atom';
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<Box flex={1} />);
+  renderer.create(<Box flex={1} width={40} />);
 });

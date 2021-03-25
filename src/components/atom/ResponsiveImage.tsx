96d151108d7e8cc1bf7ccf17991d454b443340a0
adeyahya <adeyahyaprasetyo@gmail.com>
2021-03-26 05:01:18 +0700

    responsive image

--- src/components/atom/ResponsiveImage.tsx
+++ src/components/atom/ResponsiveImage.tsx
@@ -1,12 +1,13 @@
-import {Image as BaseImage, ImageProps as BaseProps} from 'react-native';
+import {ImageProps as BaseProps} from 'react-native';
 import styled from '@emotion/native';
 import {compose, WidthProps, width, HeightProps, height} from 'styled-system';
+import BaseImage from 'react-native-scalable-image';
 
 // @ts-ignore
 export interface ImageProps extends BaseProps, WidthProps, HeightProps {}
 
 export const imageStyle = compose(width, height);
 
-const Image = styled(BaseImage)<ImageProps>(imageStyle);
+const ResponsiveImage = styled(BaseImage)<ImageProps>(imageStyle);
 
-export default Image;
+export default ResponsiveImage;
--- src/components/atom/index.ts
+++ src/components/atom/index.ts
@@ -4,3 +4,4 @@ export {default as Input} from './Input';
 export {default as Button} from './Button';
 export {default as Text} from './Text';
 export {default as Image} from './Image';
+export {default as ResponsiveImage} from './ResponsiveImage';
